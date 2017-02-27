dd659164aa09cf1fe3cfe364242a792bbcb536eb
hossien5240 <me2nr@cheaphub.net>
2017-02-27 14:49:50 +0330

    Update

--- a.php
+++ a.php
@@ -1,4 +1,4 @@
 <?php
-$token = "272680010:AAFdKC9UO-TafKuC2_nN85jI4_CZStL4JFo";
+$token = "345244468:AAFAtK5RA1JJFYArq8qFf413Y8D7pX4j56M";
 $userbot = json_decode(file_get_contents('https://api.telegram.org/bot'.$token .'/getme'));
 print_r($userbot->result->username);
